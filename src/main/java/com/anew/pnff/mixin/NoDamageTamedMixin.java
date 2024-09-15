82d251c86fe93dd4b09c85ff4718fc371fead343
Anewri32 <anewri32@gmail.com>
2024-09-14 20:13:11 -0400

    Somes changes!
    * It was fixed that the pet was immortal, now it will receive the day as long as it is not provided by the pet's due.

--- src/main/java/com/anew/pnff/mixin/NoDamageTamedMixin.java
+++ src/main/java/com/anew/pnff/mixin/NoDamageTamedMixin.java
@@ -2,6 +2,7 @@ package com.anew.pnff.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,14 +15,15 @@ public abstract class NoDamageTamedMixin {
     @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
     public void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity entity = (LivingEntity) (Object) this;
-
-        // Verifica si la entidad es una mascota domesticada
         if (entity instanceof TameableEntity) {
             TameableEntity tameableEntity = (TameableEntity) entity;
-
             if (tameableEntity.isTamed()) {
-                // Si la entidad está domesticada, cancela el daño
-                cir.setReturnValue(false);
+                if (source.getAttacker() instanceof PlayerEntity) {
+                    PlayerEntity player = (PlayerEntity) source.getAttacker();
+                    if (player.equals(tameableEntity.getOwner())) {
+                        cir.setReturnValue(false);
+                    }
+                }
             }
         }
     }
